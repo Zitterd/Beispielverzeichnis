7e3eefc83e2a44806b86d9e6c56006b26d54dfc8
jenkins <jonas.hamers@gmx.de>
2014-11-04 16:41:46 +0100

    hex datei wird ins git abgelegt

--- Code/macro.cmake
+++ Code/macro.cmake
@@ -1,6 +1,6 @@
 # create avr hex
 MACRO(AVR_CREATE_HEX name)
-ADD_CUSTOM_COMMAND(TARGET ${name} POST_BUILD COMMAND avr-objcopy ARGS -O ihex -R.eeprom ${name} "C:/Users/Jonas Hamers/Desktop/Test/Build/test.hex")
+ADD_CUSTOM_COMMAND(TARGET ${name} POST_BUILD COMMAND avr-objcopy ARGS -O ihex -R.eeprom ${name} "C:/Users/Jonas Hamers/Desktop/Beispielverzeichnis/Skript/test.hex")
 MESSAGE(STATUS "\n ----------JONAS---------------------->Macro")
 ENDMACRO(AVR_CREATE_HEX)
 
