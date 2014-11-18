3ecc6c6e6ff2b8eb50b0556ed7e1e7e2bb92586c
jenkins <jonas.hamers@gmx.de>
2014-11-18 12:37:51 +0100

    test

--- Build/CMakeFiles/test.o.dir/cmake_clean.cmake
+++ Build/CMakeFiles/test.o.dir/cmake_clean.cmake
@@ -1,8 +1,9 @@
 file(REMOVE_RECURSE
   "CMakeFiles/test.o.dir/test.c.obj"
   "test.o.pdb"
-  "test.o"
-  "test.o.manifest"
+  "test.o.exe"
+  "test.o.exe.manifest"
+  "libtest.o.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
