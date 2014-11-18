a70e86af60b890eb9414e27d967fb73425e70f03
jenkins <jonas.hamers@gmx.de>
2014-11-18 09:10:08 +0100

    init test

--- Code/Toolchain-avr-gcc.cmake
+++ Code/Toolchain-avr-gcc.cmake
@@ -7,8 +7,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 #=============================================================================#
+
 INCLUDE_DIRECTORIES("C:/Program Files (x86)/Arduino/hardware/arduino/cores/arduino")
-#INCLUDE_DIRECTORIES("C:/Program Files (x86)/Arduino/hardware/tools/avr/avr/include")
+INCLUDE_DIRECTORIES("C:/Program Files (x86)/Arduino/hardware/tools/avr/avr/include")
 INCLUDE_DIRECTORIES("C:/Program Files (x86)/Arduino/hardware/arduino/variants/standard")
 
 
