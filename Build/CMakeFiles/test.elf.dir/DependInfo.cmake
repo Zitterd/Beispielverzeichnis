8d87c3d07d049e71719f513e700a3975475b2fb6
jenkins <jonas.hamers@gmx.de>
2014-11-18 10:25:38 +0100

    test

--- Build/CMakeFiles/test.elf.dir/DependInfo.cmake
+++ Build/CMakeFiles/test.elf.dir/DependInfo.cmake
@@ -14,11 +14,10 @@ set(CMAKE_TARGET_LINKED_INFO_FILES
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  "C:/Program Files (x86)/Arduino/hardware/arduino/variants/standard"
-  "C:/Program Files (x86)/Arduino/hardware/arduino/cores/arduino"
-  "C:/Program Files (x86)/Arduino/hardware/tools/avr/avr/include"
-  "C:/Program Files (x86)/Arduino/hardware/tools/avr/avr/include/avr"
-  "C:/Program Files (x86)/Arduino/hardware/tools/avr/lib/gcc/avr/4.3.2/avr5"
+  "C:/Users/Jonas Hamers/Documents/arduino-1.5.7-windows/arduino-1.5.7/hardware/arduino/avr/variants/standard"
+  "C:/Users/Jonas Hamers/Documents/arduino-1.5.7-windows/arduino-1.5.7/hardware/arduino/avr/cores/arduino"
+  "C:/Users/Jonas Hamers/Documents/arduino-1.5.7-windows/arduino-1.5.7/hardware/tools/avr/avr/include"
+  "C:/Users/Jonas Hamers/Documents/arduino-1.5.7-windows/arduino-1.5.7/hardware/tools/avr/lib/gcc/avr/4.8.1/avr5"
   )
 set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
 set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- Code/Toolchain-avr-gcc.cmake
+++ Code/Toolchain-avr-gcc.cmake
@@ -95,7 +95,8 @@ if(NOT ARDUINO_SDK_PATH)
             /usr/local/share/arduino*)
     elseif(WIN32)
         set(SDK_PATH_HINTS "C:\\Program Files\\Arduino"
-            "C:\\Program Files (x86)\\Arduino"
+           # "C:\\Program Files (x86)\\Arduino"
+		   "C:\\Users\\Jonas Hamers\\Documents\\arduino-1.5.7-windows\\arduino-1.5.7"
             )
     endif()
     list(SORT SDK_PATH_HINTS)
