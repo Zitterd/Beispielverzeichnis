b4376c116b0e5cabb50992413481f1ad155ce25c
jenkins <jonas.hamers@gmx.de>
2014-11-19 10:35:33 +0100

    test

--- Build/CMakeFiles/test.o.dir/DependInfo.cmake
+++ Build/CMakeFiles/test.o.dir/DependInfo.cmake
@@ -19,6 +19,7 @@ set(CMAKE_C_TARGET_INCLUDE_PATH
   "C:/Program Files (x86)/Arduino/hardware/arduino/variants/standard"
   "C:/Program Files (x86)/Arduino/hardware/tools/avr/lib/gcc/avr/4.3.2"
   "C:/Program Files (x86)/Arduino"
+  "C:/Program Files (x86)/Arduino/hardware/tools/avr/bin"
   )
 set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
 set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
