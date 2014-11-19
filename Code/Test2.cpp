27048e11dd0c4c6b5c5046e58a044284c8307140
jenkins <jonas.hamers@gmx.de>
2014-11-19 10:07:40 +0100

    test mit c++

--- Code/Test2.cpp
+++ Code/Test2.cpp
@@ -2,11 +2,29 @@
 
 #include <iostream>
 
+void setup() {
+  cout << "Test bestanden2" << endl;
+}
+
+void loop() {
+  cout << "Test bestanden3" << endl;
+}
+
+
+
 int main()
 {
 	using namespace std;
 	cout << "Test bestanden" << endl;
+	setup();
+	for(;;)
+	{
+	   loop();
+	}
 	return 0;
 }
+
+
+
 
 
