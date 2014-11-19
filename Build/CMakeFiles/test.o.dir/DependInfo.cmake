46c8342cfeb75e050c0150bb92591bd64a9edc24
jenkins <jonas.hamers@gmx.de>
2014-11-19 10:59:09 +0100

    test

--- Build/CMakeFiles/test.o.dir/DependInfo.cmake
+++ Build/CMakeFiles/test.o.dir/DependInfo.cmake
@@ -17,8 +17,8 @@ set(CMAKE_C_TARGET_INCLUDE_PATH
   "C:/Program Files (x86)/Arduino/hardware/arduino/cores/arduino"
   "C:/Program Files (x86)/Arduino/hardware/tools/avr/avr/include"
   "C:/Program Files (x86)/Arduino/hardware/arduino/variants/standard"
-  "C:/Program Files (x86)/Arduino/hardware/tools/avr/lib/gcc/avr/4.3.2"
   "C:/Program Files (x86)/Arduino"
+  "C:/Program Files (x86)/Arduino/hardware/tools/avr/lib/gcc/avr/4.3.2"
   "C:/Program Files (x86)/Arduino/hardware/tools/avr/bin"
   "C:/Program Files (x86)/Arduino/hardware/tools/avr/lib/gcc/avr/4.3.2/avr5"
   )
