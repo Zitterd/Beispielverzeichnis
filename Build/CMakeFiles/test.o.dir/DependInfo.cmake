7dd9159c9d87d053d95aadf1ba60f4d951ec3244
jenkins <jonas.hamers@gmx.de>
2014-11-20 17:02:52 +0100

    test

--- Build/CMakeFiles/test.o.dir/DependInfo.cmake
+++ Build/CMakeFiles/test.o.dir/DependInfo.cmake
@@ -1,7 +1,12 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Jonas Hamers/Desktop/Beispielverzeichnis/Code/test.c" "C:/Users/Jonas Hamers/Desktop/Beispielverzeichnis/Build/CMakeFiles/test.o.dir/test.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
