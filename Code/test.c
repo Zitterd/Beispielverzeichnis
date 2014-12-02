a3da4a0fb41b383389d87414f93348ead1ccd527
jenkins <jonas.hamers@gmx.de>
2014-12-02 12:12:02 +0100

    test

--- Code/test.c
+++ Code/test.c
@@ -5,25 +5,25 @@
  *  Author: Jonas Hamers
  */ 
  
-//#include <stdio.h>
-//#include <util/delay.h>
-//#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <avr/io.h>
 
-//#define PF7	7
-//#define LED1_OFF (PORTF&=~(1<<PF7))        
-//#define LED1_ON  (PORTF|=(1<<PF7))
+#define PF7	7
+#define LED1_OFF (PORTF&=~(1<<PF7))        
+#define LED1_ON  (PORTF|=(1<<PF7))
 
 void setup(void) 
 {
-	//	DDRF = (1 << DDF7);        //PORTF BIT 7 (A0) als Ausgang
+		DDRF = (1 << DDF7);        //PORTF BIT 7 (A0) als Ausgang
 }
 
 void loop(void) 
 {
-	//LED1_ON;
-	//_delay_ms(500);
-	//LED1_OFF;
-	//_delay_ms(500);
+	LED1_ON;
+	_delay_ms(500);
+	LED1_OFF;
+	_delay_ms(500);
 }
 
 int main(void)
