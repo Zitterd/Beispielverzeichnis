ea86e5f9d649e6a8003ce12240df3b6e15cd5fef
jenkins <jonas.hamers@gmx.de>
2014-12-03 10:20:49 +0100

    neue testklasse
    uart von philipp

--- Code/test.c
+++ Code/test.c
@@ -3,8 +3,8 @@
  *
  * Created: 08.10.2014 09:53:26
  *  Author: Jonas Hamers
- */ 
- 
+ */
+
 #include <stdio.h>
 #include <util/delay.h>
 #include <avr/io.h>
@@ -12,85 +12,75 @@
 # define USART_BAUDRATE 9600
 # define BAUD_PRESCALE ((( F_CPU / ( USART_BAUDRATE * 16UL))) - 1)
 
-#define PF7	7
-#define LED1_OFF (PORTF&=~(1<<PF7))        
+#define PF7 7
+#define LED1_OFF (PORTF&=~(1<<PF7))
 #define LED1_ON  (PORTF|=(1<<PF7))
 
-void setup(void) 
+void dio_init(void)
 {
-		DDRF = (1 << DDF7);        //PORTF BIT 7 (A0) als Ausgang
+DDRF = (1 << DDF7);        //PORTF BIT 7 (A0) als Ausgang
 }
 
-void loop(void) 
+void loop(void)
 {
-	LED1_ON;
-	_delay_ms(500);
-	LED1_OFF;
-	_delay_ms(500);
+LED1_ON;
+_delay_ms(500);
+LED1_OFF;
+_delay_ms(500);
 }
 
 
 //uart-Implementierung
-uint8_t uart_getc(void)				//nicht zwingend notwendig für die Rückmeldung ins System
+void uart_init( unsigned int baud )
 {
-    while (!(UCSR1A & (1<<RXC1)))     // warten bis Zeichen verfuegbar
-        ;
-    return UDR1;                     // Zeichen aus UDR an Aufrufer zurueckgeben
+	/* Set baud rate */
+	UBRRH1 = (unsigned char)(baud>>8);
+	UBRRL1 = (unsigned char)baud;
+	/* Enable receiver and transmitter */
+	UCSR1B = (1<<RXEN1)|(1<<TXEN1);
+	/* Set frame format: 8data, 2stop bit */
+	UCSR1C = (1<<USBS1)|(3<<UCSZ10);
 }
 
-
-
-int uart_putc(unsigned char c)
+void uart_putc( unsigned char data )
 {
-    while (!(UCSR1A & (1<<UDRE1)))  /* warten bis Senden moeglich */
-    {
-    }                             
- 
-    UDR1 = c;                      /* sende Zeichen */
-    return 0;
+	/* Wait for empty transmit buffer */
+	while ( !( UCSR1A & (1<<UDRE1)) )
+	;
+	/* Put data into buffer, sends the data */
+	UDR1 = data;
 }
 
+unsigned char uart_getc( void )
+{
+	/* Wait for data to be received */
+	while ( !(UCSR1A & (1<<RXC1)) )
+	;
+	/* Get and return received data from buffer */
+	return UDR1;
+}
 
-
-void uart_puts(char *s)
+uart_puts(char *s)
 {
     while (*s)
-    {   /* so lange *s != '\0' also ungleich dem "String-Endezeichen(Terminator)" */
+    {   /* so lange *s != '\0' also ungleich dem
+		"String-Endezeichen(Terminator)" */
         uart_putc(*s);
         s++;
     }
 }
 
-
-void uart_init(void)
-{
-	// set baud rate   
-    unsigned int baud = BAUD_PRESCALE;
-    
-    UBRR1H = (unsigned char) (baud >> 8 );
-    UBRR1L = (unsigned char)baud;
-    
-    // enable received and transmitter
-    UCSR1B = ( 1 << RXEN1 ) | ( 1 << TXEN1 );
-    
-    // set frame format ( 8data, 2stop )
-    UCSR1C = ( 1 << USBS1 ) | ( 3 << UCSZ10 );
-}
-
-
-
-
 int main(void)
 {
-	uart_init();
-	uart_puts("Test bestanden");
-	
-	//LED blinken lassen
-	setup();
-	for(;;)
-	{
-	    uart_puts("Test");
-		loop();
-	}
-	return 0;
+dio_init();
+uart_init(BAUD_PRESCALE);
+
+uart_puts("Test bestanden");
+//LED blinken lassen
+setup();
+for(;;)
+{
+loop();
+}
+return 0;
 }
