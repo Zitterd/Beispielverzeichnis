215a97bf5e97dc78b35f13b8c92ca0bd76cefe45
jenkins <jonas.hamers@gmx.de>
2015-01-09 08:59:16 +0100

    include

---
+++ Build/test/CMakeFiles/sample_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_tests.dir/cunitTest.c.obj"
+  "sample_tests.pdb"
+  "sample_tests"
+  "sample_tests.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sample_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
