14fee3b4481bd69711066f0fa860b3413c09514b
jenkins <jonas.hamers@gmx.de>
2015-01-09 10:28:21 +0100

    test

---
+++ Build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Jonas Hamers/Desktop/Beispielverzeichnis/Code/test
+# Build directory: C:/Users/Jonas Hamers/Desktop/Beispielverzeichnis/Build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(sample_tests "sample_tests")
+subdirs(gtest-1.7.0)
